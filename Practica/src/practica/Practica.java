80f3c7a88e42240409116d874fe97864dd1f883f
MrYaye <zamorabrayan0722@gmail.com>
2025-03-07 17:56:26 -0500

    Add files via upload

--- Practica/src/practica/Practica.java
+++ Practica/src/practica/Practica.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package practica;
 
-/**
- *
- * @author estudiante308
- */
 public class Practica {
 
     /**
@@ -28,5 +20,69 @@ public class Practica {
         cuenta1.retirar(15000);
         cuenta1.imprimir();
     }
-    
 }
+
+
+
+
+  /* token: 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
+    
+ Modificadores UML
+Símbolos utilizados para indicar modificadores de acceso para miembros de la clase:
+
++: Público
+-: Privado
+#: Protegido
+/: Derivado (valor calculado o derivado)
+~: Paquete (acceso predeterminado o a nivel de paquete)
+¿Cómo dibujar un diagrama de
+}
+    _______________________________________________________
+   |           Cuenta                                      |           
+   |-------------------------------------------------------|
+   |#saldo:float                                           |  
+   |#numeroConsignaciones: int = 0                         |
+   |#numeroRetiros: int = 0                                |
+   |#tasaAnual:float                                       |
+   |#comisionMensual: float = 0                            |
+   |-------------------------------------------------------| 
+   |<<constructor>>+Cuenta(float saldo, float tasaAnual)   | 
+   |+consignar(float cantidad)                             |  
+   |+retirar(float cantidad)                               | 
+   |+calcularIntereses()                                   | 
+   |+extractoMensual()                                     | 
+   |_______________________________________________________| 
+    
+    
+      _____________________________________________________
+   |           CuentaAhorros                               |
+   |-------------------------------------------------------|
+   |#Activar: boolean                                      |
+   |-------------------------------------------------------| 
+   |<<constructor>>+CuentaAhorros(float saldo, float tasa) | 
+   |+retirar(float cantidad)                               |  
+   |+consignar(float cantidad)                             | 
+   |+extractoMensual()                                     | 
+   |+imprimir()                                            | 
+   |_______________________________________________________| 
+    
+    
+    
+    ______________________________________________________
+   |           CuentaCorriente                             |
+   |-------------------------------------------------------|
+   |#sobregiro: float                                      |
+   |-------------------------------------------------------| 
+   |<<constructor>>+CuentaCorriente(float saldo, float tasa)| 
+   |+retirar(float cantidad)                               |  
+   |+consignar(float cantidad)                             | 
+   |+extractoMensual()                                     | 
+   |+imprimir()                                            | 
+   |_______________________________________________________| 
+    
+    
+    
+    
+    
+    
+*/
